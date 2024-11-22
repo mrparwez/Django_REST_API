e012eddd2abc6cd3556eadc6749178e27e935f6b
mrparwez <orcl.parwez@gmail.com>
2024-11-22 16:34:33 +0530

    Django_REST_API BASIC

---
+++ _API_/asgi.py
@@ -0,0 +1,18 @@
+"""
+ASGI config for practice project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'practice.settings')  Rename name of project 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', '_API_.settings')
+
+
+application = get_asgi_application()
